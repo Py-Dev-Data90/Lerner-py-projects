68e64bfaa1cf5205bfec36d5ceb4652ea4e884da
Paige <Paige@example.com>
2025-05-28 17:26:06 -0400

    lesson 1-3 example problem solutions

---
+++ lerner-py-project.py
@@ -0,0 +1,10 @@
+#name = input('Enter your name:')
+#print('Hello, ' + name)
+
+name = input('Enter your name!:\n')
+if name == 'Paige':
+    print('Welcome, master!')
+else:
+    print('Howdy, guest!')
+
+#turning_things_from_one_type_to_another
