61aa992542bb6af3edb7e4c15e655ca9387bcd68
Paige <Paige@example.com>
2025-06-02 16:03:59 -0400

    Previous error correction explanation

--- lerner-py-excercise1.py
+++ lerner-py-excercise1.py
@@ -1,4 +1,5 @@
 #Ask the user what today's weather is, and then if it's rain or snow, print something negative. If it's sun, print something positive. If it's neither, print that you do not know what it is.
+#My notes: Remember with our true/false condition, you need to state the surrounding variables weather, and weather, twice, otherwise, the program won't throw an error, but it won't give you the result that you want!
 weather = input('What is the weather today? : \n')
 if weather == 'rainy' or weather =='snowy':
     print('Good grief is it falling again? I really dislike it!')
