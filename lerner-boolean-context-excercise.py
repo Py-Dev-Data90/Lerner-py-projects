1b73aa4570faae58248cea9bd6f2c50accb9d54f
Paige <Paige@example.com>
2025-06-05 17:32:30 -0400

    Boolean non-empty and empty string exploration

--- lerner-boolean-context-excercise.py
+++ lerner-boolean-context-excercise.py
@@ -1,9 +1,17 @@
-x = None
-#here we don't have to use == here, or 'if x is' (remember is and if are part of the language), we can just do the basic check
-if x:
-    print('x is None')
-else:
-    print('x is not None')
+# x = None
+# #here we don't have to use == here, or 'if x is' (remember is and if are part of the language), we can just do the basic check
+# if x:
+#     print('x is None')
+# else:
+#     print('x is not None')
 
 #this won't work because None == True is false, so it will return "False"
 #Everything in Python, in a boolean context, is True unless specifically negating, like empty, 0 , None, False
+
+#name is a string...non-empty strings are True in a boolean context, thus we can do:
+
+name = input('Enter your name: ')
+if name:
+    print('Hello, ' + name)
+else:
+    print('Hey! You did not enter a name!')
