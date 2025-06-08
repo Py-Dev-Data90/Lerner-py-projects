03d3a8bf82ba27f8288bd9b21e68c8adb70c0577
Paige <Paige@example.com>
2025-06-08 17:52:25 -0400

    Right side vs. left side evaluation

--- lerner-integer-exercises.py
+++ lerner-integer-exercises.py
@@ -5,3 +5,13 @@ y = 3
 
 result = x / y
 print(result)
+
+#two forward slashes means that it will return a whole number, in this case, 3
+
+# two asterisks raises it to the y power
+
+# a percentage symbol is the modulo, the remainder after taking x/y 
+
+#remember in Python, right side is evaluated before the left
+
+#  x += 1 is almost the same as x = x + 1
