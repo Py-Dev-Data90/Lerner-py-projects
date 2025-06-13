664a2c7bf9343e9354c0ae3a9f1968519dcf7a90
Paige <Paige@example.com>
2025-06-13 11:56:40 -0400

    creating converting to integers file

---
+++ lerner-converting-to-ints-excercises.py
@@ -0,0 +1,6 @@
+#python will never guess, thus, it will not guess what type of  is inside a string
+x = 1
+y = '2'
+
+# x+y would throw a type error, then, so we use int() to turn y into a different type, from a string to an integer
+#int()  aka nothing, gives you zero
