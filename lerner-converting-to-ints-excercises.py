35ab16b3710d370aba8b1d00389333cf2d095164
Paige <Paige@example.com>
2025-06-13 12:02:53 -0400

    int of floats

--- lerner-converting-to-ints-excercises.py
+++ lerner-converting-to-ints-excercises.py
@@ -4,3 +4,5 @@ y = '2'
 
 # x+y would throw a type error, then, so we use int() to turn y into a different type, from a string to an integer
 #int()  aka nothing, gives you zero
+
+# in the int of a float, int(1.5), just the int portion would be returned, so 1.
