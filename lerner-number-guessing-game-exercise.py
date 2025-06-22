75a84101b76f4e9b13e43da5780855d4367c388b
Paige <private>
2025-06-22 16:32:02 -0400

    created guessing game with int, not print

--- lerner-number-guessing-game-exercise.py
+++ lerner-number-guessing-game-exercise.py
@@ -1,4 +1,22 @@
 #Generate a random number from 1-100
+import random
+number = random.randint(1,100)
+print(number)
+# be careful not to make the mistake of using print, input(...) asks the user for input and returns a string.
+
+#You’re wrapping that in print(...), which prints the string—but print() always returns None.
+#so you’re assigning guess = None, and then comparing None < number, which raises a TypeError.that Compare the user's guess to the generated number
+guess = int(input('Please guess a number between 1 and 1 hundred: \n'))
+
+if guess < number:
+    print('Your guess was too low!')
+elif guess > number:
+    print('Your guess was too high!')
+elif guess == number:
+    print('Your guess was just right, congrats!')
+
+
+
 #Print the number (for easier debugging)
 #Ask the user to guess the number
 #Print that the guess was too high / too low / just right!
