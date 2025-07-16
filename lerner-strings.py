0d5ea8aeb2e43e7ff20b1e83ac04d8953eea8354
Paige <Paige@example.com>
2025-07-16 15:46:26 -0400

    fun mini string slicing program

--- lerner-strings.py
+++ lerner-strings.py
@@ -1,3 +1,9 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 print(alphabet)
-print(len(alphabet))
+
+length = len(alphabet)
+print(length)
+#length//2 calculates the midpoint of the string
+#alphabet[:length // 2] slices from the start up to (but not including) the midpoint
+half_alphabet= alphabet[:length//2]
+print(half_alphabet)
