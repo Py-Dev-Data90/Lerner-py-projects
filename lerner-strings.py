6424dfc342f31637360bc3b541d5c1177e848ddf
Paige <Paige@example.com>
2025-07-17 10:04:32 -0400

    floor definition

--- lerner-strings.py
+++ lerner-strings.py
@@ -12,7 +12,7 @@ def halver(alphabet):
     print(half_alphabet)
 halver(test_str)
 halver('a')
-#program returns an empty string because it floors 0.5 to 0, up to but not including a? nothing, so an empty string.
+#program returns an empty string because it floors(rounds it down) 0.5 to 0, up to but not including a? nothing, so an empty string.
 #index is the position of characters, indices is the plural form
 #remember that the indices/index of characters start at zero in almost all computer languages
 halver('abcd')
