880fc5084d061ca5954826c686403b2b59030135
Paige <Paige@example.com>
2025-08-13 15:33:22 -0400

    backslash types and raw strings to counteract escape sequences

--- lerner-backslash.py
+++ lerner-backslash.py
@@ -1,1 +1,2 @@
 #EOL means End of Line, and backslash n means go down a line, backslash t means tab over, backslash backslash becomes a new character, backslash v is vertical tab
+#Additionally, backslash a is an alarm bell, so the solution to any of these would be //a, which would turn it into a character rather than a shortcut
---
+++ lerner-raw-strings.py
@@ -0,0 +1,4 @@
+#A raw string means nothing more or nothing less than 'let's just double all the backslashes', so to do so, just put an 'r' in front of the string
+#If you need a path in windows, than you should really use a raw string
+#basically, In Python, a raw string—denoted by prefixing the string with r—treats backslashes (\) as literal characters, making it especially useful for file paths and regular expressions where escape sequences would otherwise cause errors.
+
