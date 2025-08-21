80081f4044236f867b12e69284c6729ef5f2e406
Paige <Paige@example.com>
2025-08-21 15:14:53 -0400

    the pig_lator translator

--- lerner-pig-latin-project.py
+++ lerner-pig-latin-project.py
@@ -1,5 +1,16 @@
-#Guidelines:
-#(1) If the first letter is a vowel -- a, e , i , o , u --then add way to the end
-#(2)If the first letter is *not* a vowl, then move the first letter to the end of the word, and add 'ay'
-#(3) ask the user to enter one word in English, an importance placed on capital letters 
-#(4) print the translation into Pig Latin
+# Guidelines:
+# (1) If the first letter is a vowel -- a, e, i, o, u -- then add 'way' to the end
+# (2) If the first letter is *not* a vowel, move the first letter to the end and add 'ay'
+# (3) Ask the user to enter one word in English, with importance placed on capital letters
+# (4) Print the translation into Pig Latin
+
+def pig_lator():
+    vowels = 'aeiouAEIOU'
+    user_input = input('Enter thy piggy phrase, with capital letters and proper punctuation please: ')
+    
+    first_letter = user_input[0]
+    
+    if first_letter in vowels:
+        print(user_input + 'way')
+    else:
+        print(user_input[1:] + first_letter + 'ay')
