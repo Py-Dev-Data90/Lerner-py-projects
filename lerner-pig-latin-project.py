adef0d0cf64cacebbfbf3557b3bf1432676a75ea
Paige <Paige@example.com>
2025-08-21 14:54:00 -0400

    guidelines for the pig latin project

---
+++ lerner-pig-latin-project.py
@@ -0,0 +1,5 @@
+#Guidelines:
+#(1) If the first letter is a vowel -- a, e , i , o , u --then add way to the end
+#(2)If the first letter is *not* a vowl, then move the first letter to the end of the word, and add 'ay'
+#(3) ask the user to enter one word in English, an importance placed on capital letters 
+#(4) print the translation into Pig Latin
