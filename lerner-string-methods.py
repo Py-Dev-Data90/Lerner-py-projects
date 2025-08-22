8cd26cb4719bb15019d34630d2ea92bbc04d50db
Paige <Paige@example.com>
2025-08-22 17:57:05 -0400

    you can use more than one string method!

--- lerner-string-methods.py
+++ lerner-string-methods.py
@@ -1,2 +1,5 @@
 #Strip returns something without spaces, remember, it does not affect name since strings are immutable, it returns a new string
 #if we want to change a string, we have to reassign it a new value, such as x = 'potato'
+
+name = input('Enter your name:').strip().capitalize()
+print('Hello,' + name + '!')
