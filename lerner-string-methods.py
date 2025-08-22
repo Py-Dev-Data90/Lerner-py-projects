a4fc6f537b6bb53a537cecd7ca652baef98bd07e
Paige <Paige@example.com>
2025-08-22 17:58:16 -0400

    method chaining

--- lerner-string-methods.py
+++ lerner-string-methods.py
@@ -1,5 +1,5 @@
 #Strip returns something without spaces, remember, it does not affect name since strings are immutable, it returns a new string
 #if we want to change a string, we have to reassign it a new value, such as x = 'potato'
-
+#This is called method chaining, where you use more than one string method in a row!
 name = input('Enter your name:').strip().capitalize()
 print('Hello,' + name + '!')
